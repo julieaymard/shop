5a8d2dbd3f30c98531d365f9b303fa9bcf313162
Hunter1148 <hedjouliayoub@gmail.com>
2018-03-05 16:28:59 +0100

    Api Application ws

--- farm-java/src/main/java/io/pax/farm/domain/jdbc/SimpleClient.java
+++ farm-java/src/main/java/io/pax/farm/domain/jdbc/SimpleClient.java
@@ -23,4 +23,9 @@ public class SimpleClient implements Client {
     public String getMail() {
         return this.mail;
     }
+
+    @Override
+    public String toString() {
+        return this.mail;
+    }
 }
