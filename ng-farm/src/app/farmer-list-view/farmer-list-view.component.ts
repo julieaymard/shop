b49ac4b72d26adf7c6ab302e8c1cc4127cba3ce8
julieaymard <julie.lepesant@free.fr>
2018-03-06 14:51:21 +0100

    front (missing souscription, pick some vegetables, pick a date, order) !! FARMER VIEW

---
+++ ng-farm/src/app/farmer-list-view/farmer-list-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-farmer-list-view',
+  templateUrl: './farmer-list-view.component.html',
+  styleUrls: ['./farmer-list-view.component.css']
+})
+export class FarmerListViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
