eb926ac68681eba65d37d40ea4e75c009df056f8
julieaymard <julie.lepesant@free.fr>
2018-03-06 14:23:35 +0100

    front (missing command and choose date)

--- ng-farm/src/app/data-service.service.ts
+++ ng-farm/src/app/data-service.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {Client} from "./model/client";
 import {HttpClient} from "@angular/common/http";
 import {Product} from "./model/products";
+import {Date} from "./model/dates";
 
 @Injectable()
 export class Dataservice {
@@ -25,6 +26,13 @@ export class Dataservice {
       .then(data => data as Product[])
   }
 
+  fetchDates(): Promise<Date[]> {
+
+    return this.http
+      .get('http://localhost:8080/farm-java/api/dates')
+      .toPromise()
+      .then(data => data as Date[])
+  }
 
 
 }
